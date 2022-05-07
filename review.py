43128a19048f17c31c237aea3bc54b7a5e529b8f
Charlene Lo <lo861119@gmail.com>
2022-05-07 11:36:44 +0800

    add review filter

--- review.py
+++ review.py
@@ -1,6 +1,8 @@
 
 data = []
 count = 0
+
+# 讀取留言，並計算總數
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -9,12 +11,18 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')	
 
+# 計算留言平均長度
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言平均長度為', sum_len/len(data))
 
-
+# 篩選小於100個字的留言
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '比留言長度小於100')
 
 
 
